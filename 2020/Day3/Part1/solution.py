7359f0c2341781a822310591256d170f627ca0c1
Andres Vertiz <andre.vertiz@gmail.com.com>
2020-12-03 00:47:17 -0600

    day3

---
+++ 2020/Day3/Part1/solution.py
@@ -0,0 +1,88 @@
+# --- Day 3: Toboggan Trajectory ---
+# With the toboggan login problems resolved, you set off toward the airport. 
+# While travel by toboggan might be easy, it's certainly not safe: there's very minimal steering and the area is covered in trees. 
+# You'll need to see which angles will take you near the fewest trees.
+
+# Due to the local geology, trees in this area only grow on exact integer coordinates in a grid. 
+# You make a map (your puzzle input) of the open squares (.) and trees (#) you can see. For example:
+
+# ..##.......
+# #...#...#..
+# .#....#..#.
+# ..#.#...#.#
+# .#...##..#.
+# ..#.##.....
+# .#.#.#....#
+# .#........#
+# #.##...#...
+# #...##....#
+# .#..#...#.#
+
+# These aren't the only trees, though; due to something you read about once involving arboreal genetics and biome stability, the same pattern repeats to the right many times:
+
+# ..##.........##.........##.........##.........##.........##.......  --->
+# #...#...#..#...#...#..#...#...#..#...#...#..#...#...#..#...#...#..
+# .#....#..#..#....#..#..#....#..#..#....#..#..#....#..#..#....#..#.
+# ..#.#...#.#..#.#...#.#..#.#...#.#..#.#...#.#..#.#...#.#..#.#...#.#
+# .#...##..#..#...##..#..#...##..#..#...##..#..#...##..#..#...##..#.
+# ..#.##.......#.##.......#.##.......#.##.......#.##.......#.##.....  --->
+# .#.#.#....#.#.#.#....#.#.#.#....#.#.#.#....#.#.#.#....#.#.#.#....#
+# .#........#.#........#.#........#.#........#.#........#.#........#
+# #.##...#...#.##...#...#.##...#...#.##...#...#.##...#...#.##...#...
+# #...##....##...##....##...##....##...##....##...##....##...##....#
+# .#..#...#.#.#..#...#.#.#..#...#.#.#..#...#.#.#..#...#.#.#..#...#.#  --->
+
+# You start on the open square (.) in the top-left corner and need to reach the bottom (below the bottom-most row on your map).
+
+# The toboggan can only follow a few specific slopes (you opted for a cheaper model that prefers rational numbers); 
+# start by counting all the trees you would encounter for the slope right 3, down 1:
+
+# From your starting position at the top-left, check the position that is right 3 and down 1. 
+# Then, check the position that is right 3 and down 1 from there, and so on until you go past the bottom of the map.
+
+# The locations you'd check in the above example are marked here with O where there was an open square and X where there was a tree:
+
+# ..##.........##.........##.........##.........##.........##.......  --->
+# #..O#...#..#...#...#..#...#...#..#...#...#..#...#...#..#...#...#..
+# .#....X..#..#....#..#..#....#..#..#....#..#..#....#..#..#....#..#.
+# ..#.#...#O#..#.#...#.#..#.#...#.#..#.#...#.#..#.#...#.#..#.#...#.#
+# .#...##..#..X...##..#..#...##..#..#...##..#..#...##..#..#...##..#.
+# ..#.##.......#.X#.......#.##.......#.##.......#.##.......#.##.....  --->
+# .#.#.#....#.#.#.#.O..#.#.#.#....#.#.#.#....#.#.#.#....#.#.#.#....#
+# .#........#.#........X.#........#.#........#.#........#.#........#
+# #.##...#...#.##...#...#.X#...#...#.##...#...#.##...#...#.##...#...
+# #...##....##...##....##...#X....##...##....##...##....##...##....#
+# .#..#...#.#.#..#...#.#.#..#...X.#.#..#...#.#.#..#...#.#.#..#...#.#  --->
+
+# In this example, traversing the map using this slope would cause you to encounter 7 trees.
+
+# Starting at the top-left corner of your map and following a slope of right 3 and down 1, how many trees would you encounter?
+
+import pandas as pd
+
+def row_len_check(row, position):
+    if len(row) <= position + 3:
+        row *= position
+        return(row)
+    else:
+        return(row)
+
+def traverse(df, row_num, position, trees):
+    row = df['map'].iloc[row_num]
+    row = row_len_check(row = row, position = position)
+    if row_num == df.shape[0]-1:
+        if row[position] == '#':
+            trees += 1
+        return(trees)
+    else:
+        if row[position] == '#':
+            trees += 1
+        return traverse(df = df, row_num = row_num + 1, position = position + 3, trees = trees)
+
+def main():
+    input = pd.read_csv('input.csv')
+    trees = traverse(df = input, row_num = 0, position = 0, trees = 0)
+    print(trees)
+
+if __name__ == "__main__":
+    main()
---
+++ 2020/Day3/Part2/solution.py
@@ -0,0 +1,52 @@
+# --- Part Two ---
+# Time to check the rest of the slopes - you need to minimize the probability of a sudden arboreal stop, after all.
+
+# Determine the number of trees you would encounter if, for each of the following slopes, you start at the top-left corner and traverse the map all the way to the bottom:
+
+# Right 1, down 1.
+# Right 3, down 1. (This is the slope you already checked.)
+# Right 5, down 1.
+# Right 7, down 1.
+# Right 1, down 2.
+# In the above example, these slopes would find 2, 7, 3, 4, and 2 tree(s) respectively; multiplied together, these produce the answer 336.
+
+# What do you get if you multiply together the number of trees encountered on each of the listed slopes?
+
+import pandas as pd
+
+def row_len_check(row, position):
+    if len(row) <= position + 7:
+        row *= position
+        return(row)
+    else:
+        return(row)
+
+def traverse(df, row_num, position, trees, slope):
+    row = df['map'].iloc[row_num]
+    row = row_len_check(row = row, position = position)
+    if row_num == df.shape[0]-1:
+        if row[position] == '#':
+            trees += 1
+        return(trees)
+    else:
+        if row[position] == '#':
+            trees += 1
+        return traverse(df = df, row_num = row_num + slope[0], position = position + slope[1], trees = trees, slope = slope)
+
+def main():
+    input = pd.read_csv('input.csv')
+    slope = [1,1]
+    slope1 = traverse(df = input, row_num = 0, position = 0, trees = 0, slope = slope)
+    slope = [1,3]
+    slope2 = traverse(df = input, row_num = 0, position = 0, trees = 0, slope = slope)
+    slope = [1,5]
+    slope3 = traverse(df = input, row_num = 0, position = 0, trees = 0, slope = slope)
+    slope = [1,7]
+    slope4 = traverse(df = input, row_num = 0, position = 0, trees = 0, slope = slope)
+    slope = [2,1]
+    slope5 = traverse(df = input, row_num = 0, position = 0, trees = 0, slope = slope)
+    trees = slope1 * slope2 * slope3 * slope4 * slope5
+    print(trees)
+
+if __name__ == "__main__":
+    main()
